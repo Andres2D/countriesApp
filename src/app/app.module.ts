c03aa6b5f291623f6d24e4449064765807e6c0ae
andres.alcaraz <andres.alcaraz@numrot.com>
2021-02-17 20:19:44 -0500

    creating basic structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CountryModule } from './country/country.module';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    CountryModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/country/pages/by-capital/by-capital.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital',
+  templateUrl: './by-capital.component.html',
+  styles: []
+})
+export class ByCapitalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
