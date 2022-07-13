5d03e8deb8c8b5dd89c2bb5db3c10d19889ae948
Andres2D <andres2d1997.1206@gmail.com>
2022-07-12 19:51:03 -0500

    Fix country page formats!

--- src/app/country/pages/see-country/see-country.component.ts
+++ src/app/country/pages/see-country/see-country.component.ts
@@ -1,32 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap, tap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 
 import { CountryService } from '../../services/country.service';
-import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-see-country',
   templateUrl: './see-country.component.html',
-  styles: []
+  styleUrls: ['./see-country.component.css']
 })
 export class SeeCountryComponent implements OnInit {
 
-  country!: Country;
+  country!: any;
   mapUrl = '';
+  countryNativeName: any = '';
+  countryFemonymsM: any = '';
+  countryFemonymsF: any = '';
 
   constructor(private activateRoute: ActivatedRoute, private countryService: CountryService) { }
 
   ngOnInit() {
-      this.activateRoute.params
-        .pipe(
-          switchMap(( param ) => this.countryService.SearchCountry(param.id))
-        )
-        .subscribe(country => this.country = country)
+    this.activateRoute.params
+      .pipe(
+        switchMap(( param ) => this.countryService.SearchCountry(param.id))
+      )
+      .subscribe(country => {
+        this.country = country[0];
+        this.countryNativeName = Object.entries(this.country.name.nativeName)[0][1];
+        this.countryFemonymsM = Object.entries(this.country.demonyms)[0][1];
+        this.countryFemonymsF = Object.entries(this.country.demonyms)[0][1];
+      })
   }
 
   GoToMap(){
     window.open( 
-      `https://www.google.com/maps/search/?api=1&query=${this.country.name}`, "_blank");
+      `https://www.google.com/maps/search/?api=1&query=${this.country.name.common}`, "_blank");
   }
 }
